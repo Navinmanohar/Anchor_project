6e9ad2bf57419f0e420506add791575653d2d190
navinmanohar <navinmanohar78086@gmail.com>
2024-01-27 09:46:13 +0530

    port added

--- src/Api/MessageRequests.js
+++ src/Api/MessageRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://backend-anchor.vercel.app/' });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 
